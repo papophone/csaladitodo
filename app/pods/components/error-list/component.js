8873bea2446be496afce9863f04fb80e32cda650
papophone <papophone@gmail.com>
2016-01-07 09:23:58 +0000

    csaladitodo

---
+++ app/pods/components/error-list/component.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    actions : {
+        deleteError(error) {
+            error.deleteRecord();
+            error.save();
+        }
+    }
+});
